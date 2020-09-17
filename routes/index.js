1902fa5889d4d587baee78ab04b3a8a91809cc2e
A. K. M. Saiful Islam <saifulislamadar@gmail.com>
2020-09-18 03:48:57 +0600

    added jwt verification

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,22 @@
 var express = require("express");
 var router = express.Router();
-
+var jwtAuthenticator = require("../middlewares/jwt_authenticator")();
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.json("I am here");
 });
 
-router.get("/gen", function (req, res, next) {
-  let token = require("crypto").randomBytes(64).toString("hex");
-  console.log(token);
-  res.json("Ok");
+router.get("/gen", async (req, res, next) => {
+  const token = await jwtAuthenticator.generateToken();
+  res.json(token);
 });
+
+router.post(
+  "/verify",
+  jwtAuthenticator.authenticateToken,
+  async (req, res, next) => {
+    res.json(req.user.user);
+  }
+);
 
 module.exports = router;
